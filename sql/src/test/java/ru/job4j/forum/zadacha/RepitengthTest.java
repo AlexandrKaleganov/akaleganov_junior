e9bfd060b5677d15314e800f334a288234635b31
AlexandrKaleganov <alexmur07@mail.ru>
2018-11-21 22:58:04 +0700

    Задачи для форума Найти самую длинную подпоследовательность полностью различных подряд идущих элементов массива

--- sql/src/test/java/ru/job4j/forum/zadacha/RepitengthTest.java
+++ sql/src/test/java/ru/job4j/forum/zadacha/RepitengthTest.java
@@ -12,7 +12,7 @@ public class RepitengthTest {
         int[] ints = {5, 7, 6, 6, 8, 1, 2, 2, 7, 4, 3}; //7 4
         int[] ints1 = {5, 7, 7, 4, 3, 8, 2, 2, 7, 1};   //3  5
         int[] ints2 = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};  //0
-        int[] ints3 = {1, 2, 3, 1, 1, 1, 1, 2, 3};          //0  4
+        int[] ints3 = {1, 2, 3, 1, 1, 1, 1, 2, 3};          //6 3
         repitength.sizemass(ints);
         repitength.sizemass(ints1);
         repitength.sizemass(ints2);
