ccb3c271cdc80413a511167586b590151aad0d1e
AlexandrKaleganov <alexmur07@mail.ru>
2018-11-23 22:43:23 +0700

    Сервлеты промышленное программирование

---
+++ servletjsp/src/main/java/ru/job4j/kniga/MyServlet.java
@@ -0,0 +1,6 @@
+package ru.job4j.kniga;
+
+import javax.servlet.http.HttpServlet;
+
+public class MyServlet extends HttpServlet {
+}
--- sql/src/test/java/ru/job4j/forum/zadacha/RepitengthTest.java
+++ sql/src/test/java/ru/job4j/forum/zadacha/RepitengthTest.java
@@ -9,11 +9,11 @@ public class RepitengthTest {
     @Test
     public void sizemass() {
         Repitength repitength = new Repitength();
-        int[] ints = {5, 7, 6, 6, 8, 1, 2, 2, 7, 4, 3}; //7 4
-        int[] ints1 = {5, 7, 7, 4, 3, 8, 2, 2, 7, 1};   //2  5
-        int[] ints2 = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};  //0
-        int[] ints3 = {1, 2, 3, 1, 1, 1, 1, 2, 3};          //6 3
-        int[] ints4 = {4, 6, 1, 2, 1, 2, 3, 2};
+        int[] ints = {5, 7, 6, 6, 8, 1, 2, 2, 7, 4, 3}; //индекс 7 размер 4
+        int[] ints1 = {5, 7, 7, 4, 3, 8, 2, 2, 7, 1};   //индекс 2  размер 5
+        int[] ints2 = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};  // 0
+        int[] ints3 = {1, 2, 3, 1, 1, 1, 1, 2, 3};          // индекс 6 размер 3
+        int[] ints4 = {4, 6, 1, 2, 1, 2, 3, 2};        //индекс 0  размер 4
         repitength.siseres(ints);
         repitength.siseres(ints1);
         repitength.siseres(ints2);
