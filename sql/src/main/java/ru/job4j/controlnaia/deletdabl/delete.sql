9bc4b8ce3730da69868df07e617c42c9dd8e4fdd
AlexandrKaleganov <alexmur07@mail.ru>
2018-11-29 22:09:45 +0700

    скрипт удаления

--- sql/src/main/java/ru/job4j/controlnaia/deletdabl/delete.sql
+++ sql/src/main/java/ru/job4j/controlnaia/deletdabl/delete.sql
@@ -1,5 +1,5 @@
 ﻿--добавим объекты в нашу таблицу объекты с цифрами 2 и 3 дублироваться будут
---insert into items (name) values('object1'), ('object1'), ('object2'),('object3'),('object3');
+--insert into items (name) values('1'), ('2'), ('3'),('1');
 --select * from items;
 --вот так мы можем найти все дубли и удалим:
 delete from items where name in(select name from items group by name having count(*) > 1);
