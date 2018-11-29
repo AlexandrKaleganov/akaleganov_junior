15eba3b5e3d01388b122bab3e013987b9399f82f
AlexandrKaleganov <alexmur07@mail.ru>
2018-11-29 22:58:39 +0700

    Сервлет начало

---
+++ servletjsp/src/main/java/ru/job4j/architecture/UserServlet.java
@@ -0,0 +1,40 @@
+package ru.job4j.architecture;
+
+import ru.job4j.architecture.err.DatabaseException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class UserServlet extends HttpServlet {
+    private final Validate validate = ValidateService.getInstance();
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(res.getOutputStream());
+        String r;
+        try {
+            r = validate.findAll().toString();
+        } catch (DatabaseException e) {
+            r = e.getMessage();
+        }
+        writer.append(r);
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String res;
+        try {
+            res = this.validate.add(new Users(req.getParameter("id"), req.getParameter("Name"), req.getParameter("Login")));
+        } catch (DatabaseException e) {
+            res = e.getMessage();
+        }
+        doGet(req, resp);
+    }
+}
