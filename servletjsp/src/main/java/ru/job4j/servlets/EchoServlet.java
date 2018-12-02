acb9a94d32a7a9f3864f2a7e139ab252bd5ab273
AlexandrKaleganov <alexmur07@mail.ru>
2018-12-02 16:49:31 +0700

    Сервлет echo

--- servletjsp/src/main/java/ru/job4j/servlets/EchoServlet.java
+++ servletjsp/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -1,4 +1,5 @@
 package ru.job4j.servlets;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,18 +12,43 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class EchoServlet extends HttpServlet {
     private ConcurrentHashMap<String, String> name = new ConcurrentHashMap<>();
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setContentType("text/html");
+        res.setContentType("text/html; charset=windows-1251");
         PrintWriter writer = new PrintWriter(res.getOutputStream());
-        writer.append("hello world, " + this.name);
+        StringBuilder bilder = new StringBuilder("<table border='1'>");
+        bilder.append("<caption>список</caption>");
+        bilder.append("<tr>\n"
+                + "    <th><b>орол<b></th>\n"
+                + "    <th>NAME</th>\n"
+                + "   </tr>");
+        this.name.forEach((k, n) -> bilder.append("<tr><td>" + k + "</td>" + "<td>" + n + "</td></tr>"));
+        bilder.append("</table>");
+        writer.append("<!DOCTYPE html>\n"
+                + "<html lang='en'>\n"
+                + "<head>\n"
+                + "    <meta charset='UTF-16'>\n"
+                + "    <title></title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<form action='" + req.getContextPath() + "/echo' method='post'>"
+                + "<input type='hidden' name='id' value='<%=id%>'/>"
+                + "key : <input type='text' name='key'/>"
+                + "Name : <input type='text' name='login'/>"
+                + "<input type='submit'>"
+                + "</form>"
+                + "<br/>"
+                + bilder
+                + "</body>\n"
+                + "</html>");
         writer.flush();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        this.name.put(req.getParameter("num"), req.getParameter("login"));
+        this.name.put(req.getParameter("key"), req.getParameter("login"));
         doGet(req, resp);
     }
 }
