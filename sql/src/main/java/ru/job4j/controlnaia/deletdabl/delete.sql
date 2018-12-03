c488880995d0b0ed50ec9782119bae89df902798
AlexandrKaleganov <alexmur07@mail.ru>
2018-12-03 21:08:06 +0700

    SQL  запрос удаление дублей

--- sql/src/main/java/ru/job4j/controlnaia/deletdabl/delete.sql
+++ sql/src/main/java/ru/job4j/controlnaia/deletdabl/delete.sql
@@ -1,7 +1,6 @@
 ﻿--добавим объекты в нашу таблицу объекты с цифрами 2 и 3 дублироваться будут
---insert into items (name) values('1'), ('2'), ('3'),('1');
+--insert into items (name) values('1'), ('2'), ('3'),('1'), ('8'),('8'),('8');
 --select * from items;
 --вот так мы можем найти все дубли и удалим:
-delete from items where name in(select name from items group by name having count(*) > 1);
-
+delete from items where id in (select i.id from items as i left outer join (select max(id), name from items group by name having count(*) > 1 ) as z on i.name = z.name where z.name is not null and i.id != z.max);
 select * from items;
