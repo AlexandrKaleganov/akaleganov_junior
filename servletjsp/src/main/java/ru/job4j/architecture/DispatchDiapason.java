54ccd419f7ef2dfa12cd3ee6c424aa311851b851
AlexandrKaleganov <alexmur07@mail.ru>
2018-12-06 22:36:24 +0700

    Сервлеты перезалив

---
+++ servletjsp/src/main/java/ru/job4j/architecture/DispatchDiapason.java
@@ -0,0 +1,83 @@
+package ru.job4j.architecture;
+/**
+ * класс диспетчер комманд
+ * в хешмапе содержаться три команды
+ * add, update, delete  и реализованы они через лямбы
+ * с помощью функционального интерфейса
+ * глядя на тот шаблон, который предоставляется в задании я ничего лучше не смог придумать надеюсь подход правильный
+ * метод init  инициализирует нашу мапу
+ * метод access получает доступ к нужной функции на входи идёт валидатор, ключ и созданный пользователь
+ */
+
+import ru.job4j.architecture.err.DatabaseException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class DispatchDiapason {
+    /**
+     * Dispatch.
+     */
+
+    private final Map<String, Function<Users, Optional>> dispatch = new HashMap<String, Function<Users, Optional>>();
+    private final Validate validate = ValidateService.getInstance();
+    private final static DispatchDiapason INSTANCE = new DispatchDiapason().init();
+
+    public static DispatchDiapason getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Load initial handlers.
+     *
+     * @return current object.
+     */
+    public DispatchDiapason init() {
+        this.dispatch.put("add", (users) -> {
+                    try {
+                        return Optional.of(this.validate.add(users));
+                    } catch (DatabaseException e) {
+                        return Optional.of(e.getMessage());
+                    }
+                }
+        );
+        this.dispatch.put("update", (users) -> {
+            try {
+                return Optional.of(this.validate.update(users).toString());
+            } catch (DatabaseException e) {
+                return Optional.of(e.getMessage());
+            }
+        });
+        this.dispatch.put("delete", (users) -> {
+                    try {
+                        return Optional.of(this.validate.delete(users));
+                    } catch (DatabaseException e) {
+                        return Optional.of(e.getMessage());
+                    }
+                }
+        );
+        this.dispatch.put("findall", (users) ->
+                Optional.of(this.validate.findAll())
+        );
+        this.dispatch.put("findbyid", (users) -> {
+                    try {
+                        return Optional.of(this.validate.findById(users));
+                    } catch (DatabaseException e) {
+                        return Optional.of(e.getMessage());
+                    }
+                }
+        );
+        return this;
+    }
+
+    /**
+     * Check access for person by age.
+     *
+     * @return true if access are allowed
+     */
+    public Optional access(String key, Users users) {
+        return this.dispatch.get(key).apply(users);
+    }
+}
---
+++ servletjsp/src/main/java/ru/job4j/architecture/UserCreateServlet.java
@@ -0,0 +1,51 @@
+package ru.job4j.architecture;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class UserCreateServlet extends HttpServlet {
+    private final DispatchDiapason dispatsh = DispatchDiapason.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        req.setCharacterEncoding("utf-8");
+        PrintWriter print = resp.getWriter();
+
+        StringBuilder result = new StringBuilder("<!DOCTYPE html>\n"
+                + "<html lang='en'>\n"
+                + "<head>\n"
+                + "    <meta charset='UTF-8'>\n"
+                + "    <title></title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<br/>"
+                + "<form action='" + req.getContextPath() + "/list' method='post'>\n"
+                + "<input type='hidden' name='action' value='add'/>"
+                + "ID : <input type='text' name='id' value=''/>"
+                + "Name : <input type='text' name='name' value=''/>"
+                + "Login : <input type='text' name='login' value=''/>"
+                + "<input type='submit' value='ДОБАВИТЬ'>"
+                + "</form>"
+                + "<br/>"
+                + "</body>\n"
+                + "</html>");
+        print.print(result);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        req.setCharacterEncoding("utf-8");
+        PrintWriter writer = resp.getWriter();
+        StringBuilder res = new StringBuilder();
+        res.append(this.dispatsh.access(req.getParameter("action"),
+                new Users(req.getParameter("id"), req.getParameter("name"), req.getParameter("login"))).get());
+        writer.print(res);
+        doGet(req, resp);
+    }
+}
