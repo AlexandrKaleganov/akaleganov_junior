ab6a873c332a2cbf76d86a870fecc987aaceffe8
AlexandrKaleganov <alexmur07@mail.ru>
2018-12-24 12:14:59 +0700

    поправил скрип создания бд

--- servletjsp/src/main/resources/createdatabaseuser.sql
+++ servletjsp/src/main/resources/createdatabaseuser.sql
@@ -1,2 +1,2 @@
-﻿--create database users;
+﻿--create database usersdata;
 create table  if not exists  users(id serial primary key, name character(200), login character(200), create_date Timestamp default now());
