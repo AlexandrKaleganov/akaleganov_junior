3dd4a41b9623e2e57305fea91bad13ac73aa60c3
AlexandrKaleganov <alexmur07@mail.ru>
2019-02-02 18:29:54 +0700

    Фильтер

--- servletjsp/src/main/java/ru/job4j/architecture/AuthFilter.java
+++ servletjsp/src/main/java/ru/job4j/architecture/AuthFilter.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AuthFilter implements Filter {
@@ -17,13 +18,13 @@ public class AuthFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(req, res);
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
         if (request.getRequestURI().contains("/signin")) {
             chain.doFilter(req, res);
-        } else if (request.getSession(false).getAttribute("login") == null) {
+        } else if (request.getSession(false) == null || request.getSession(false).getAttribute("login") == null) {
             response.sendRedirect(String.format("%s/signin", request.getContextPath()));
+            return;
         }
         chain.doFilter(req, res);
     }
