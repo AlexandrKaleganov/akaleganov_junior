c81349541845b1d158ea8c3edfd911c490e60dc7
AlexandrKaleganov <alexmur07@mail.ru>
2019-02-17 22:09:03 +0700

    Java script

--- servletjs/src/test/java/ru/job4j/bootstrap/DispatchTest.java
+++ servletjs/src/test/java/ru/job4j/bootstrap/DispatchTest.java
@@ -14,8 +14,13 @@ public class DispatchTest {
     @Test
     public void submit() {
         User user = new User(1, "fame", "name", "М", "desc");
+        StringBuilder rsl = new StringBuilder();
         Dispatch dispatch = Dispatch.getINSTANCE();
         Assert.assertThat(dispatch.submit("add", user, user), is(user));
         Assert.assertThat(dispatch.submit("findall", new User(), new ConcurrentHashMap<>()).get(1), Is.is(user));
+        ConcurrentHashMap<Integer, User> temp = dispatch.submit("findall", new User(), new ConcurrentHashMap<Integer, User>());
+        temp.forEach((k, v) ->
+                rsl.append(String.format("{id:%s, surname:%s, name:%s, sex:%s, desc:%s}",
+                        v.getId(), v.getSurname(), v.getName(), v.getSex(), v.getDesc())));
     }
 }
