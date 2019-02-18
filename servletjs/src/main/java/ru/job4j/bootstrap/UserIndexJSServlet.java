abb599b35b10351c65b2dffd83044a2b5c0cef8a
AlexandrKaleganov <alexmur07@mail.ru>
2019-02-18 16:46:20 +0700

    123

--- servletjs/src/main/java/ru/job4j/bootstrap/UserIndexJSServlet.java
+++ servletjs/src/main/java/ru/job4j/bootstrap/UserIndexJSServlet.java
@@ -6,12 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 public class UserIndexJSServlet extends HttpServlet {
     private Dispatch disp = Dispatch.getINSTANCE();
